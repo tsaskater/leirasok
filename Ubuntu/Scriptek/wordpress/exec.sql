f1cfa347e84ba083c8f128db98751383d825d1e1
tsaskater <42976086+tsaskater@users.noreply.github.com>
2019-04-17 11:00:02 +0200

    Delete exec.sql

--- Ubuntu/Scriptek/wordpress/exec.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE wordpressdb;
-CREATE USER wordpressuser@localhost IDENTIFIED BY 'wordpresspassword';
-GRANT ALL PRIVILEGES ON wordpressdb.* TO wordpressuser@localhost;
-FLUSH PRIVILEGES;
